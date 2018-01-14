425cb21d29befba9c11bb8e92f3d88c135ccd002
lost273 <dnk273@gmail.com>
2018-01-14 11:21:46 +0300

    14-01-2018

--- PersonalMirror/Scripts/myscript.js
+++ PersonalMirror/Scripts/myscript.js
@@ -1,1 +1,6 @@
-﻿alert('Привет мир!');
+﻿var strSum = prompt("Enter the sum", 1000);
+var strPercent = prompt("Enter the percentage", 10);
+var sum = parseInt(strSum);
+var procent = parseInt(strPercent);
+sum = sum + sum * procent / 100;
+alert("Deposit is: " + sum);
