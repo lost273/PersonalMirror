a11a85b6bf480d3a020adc57323746874d34c915
lost273 <dnk273@gmail.com>
2018-01-14 14:20:18 +0300

    14-01-2018

--- PersonalMirror/Scripts/myscript.js
+++ PersonalMirror/Scripts/myscript.js
@@ -1,5 +1,19 @@
-﻿var people = ["Tom", "Alice", "Bob", "Sam"];
-for (var index in people) {
+﻿var array = [99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49];
 
-    document.write(index + "</br>");
+var sort = function(a){
+    
+    for (var i = 0; i < a.length; i++){
+        for (var j = a.length - 1; j > 0; j--){
+            if (a[j-1] > a[j]) {
+                var temp;
+                temp = a[j-1];
+                a[j-1] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+}
+sort(array);
+for (var index in array) {
+    document.write(array[index] + "</br>");
 }
