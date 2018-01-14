a67a96e69fa9015a13ce87ba1129d453144221e9
lost273 <dnk273@gmail.com>
2018-01-14 13:34:50 +0300

    14-01-2018

--- PersonalMirror/Scripts/myscript.js
+++ PersonalMirror/Scripts/myscript.js
@@ -1,6 +1,5 @@
-﻿var strSum = prompt("Enter the sum", 1000);
-var strPercent = prompt("Enter the percentage", 10);
-var sum = parseInt(strSum);
-var procent = parseInt(strPercent);
-sum = sum + sum * procent / 100;
-alert("Deposit is: " + sum);
+﻿var people = ["Tom", "Alice", "Bob", "Sam"];
+for (var index in people) {
+
+    document.write(index + "</br>");
+}
