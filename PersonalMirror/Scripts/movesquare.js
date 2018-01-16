f6d88366479f931ae0291fc5cd0efef19235c548
lost273 <dnk273@gmail.com>
2018-01-16 16:51:34 +0300

    16-01-2018

--- PersonalMirror/Scripts/movesquare.js
+++ PersonalMirror/Scripts/movesquare.js
@@ -1,3 +1,5 @@
-﻿function move() {
-
+﻿function move(obj) {
+    //var articleDiv = document.querySelector("div.article");
+    document.getElementById("square-empty").id = "square";
+    obj.id = "square-empty";
 }
