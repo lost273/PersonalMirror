26e02107197462ef33f38288c7563afafbaa4f0d
lost273 <dnk273@gmail.com>
2018-01-17 19:19:00 +0300

    17-01-2018

--- PersonalMirror/Scripts/movesquare.js
+++ PersonalMirror/Scripts/movesquare.js
@@ -1,12 +1,28 @@
 ﻿function move(event) {
-    //var articleDiv = document.querySelector("div.article");
-    //document.getElementById("square-empty").id = "square";
-    //event.target.id = "square-empty";
-    event.target.id = "square1";
+    if (event.target.id == "square-empty") {
+        return;
+    }
+    var elements = document.querySelectorAll("#field.row div");
+    var indexZero, indexCurrent;
+    for (var index in elements) {
+        if (elements[index].innerText == "") {
+            indexZero = index;
+        }
+        if (elements[index].innerText == event.target.innerText) {
+            indexCurrent = index;
+        }
+    }
+    console.log(indexCurrent);
+    console.log(indexZero);
+    if (((indexCurrent - 3) == indexZero) || ((indexCurrent + 3) == indexZero) ||
+        ((indexCurrent - 1) == indexZero) || ((indexCurrent + 1) == indexZero)) {
+        elements[indexCurrent].id = "square-empty";
+        elements[indexZero].id = "square";
+    }
 }
 //document.getElementById("square-empty").addEventListener("click", move);
-var elements = document.querySelectorAll("#field.row div");
-for (var index in elements) {
-    console.log(elements[index].innerText);
-    elements[index].addEventListener("click", move);
+var el = document.querySelectorAll("#field.row div");
+for (var index in el) {
+    console.log(el[index].innerText);
+    el[index].addEventListener("click", move);
 }
