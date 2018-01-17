8b06d0c1702f2ad47e8150b32ee34ddd090f7a85
lost273 <dnk273@gmail.com>
2018-01-17 16:55:52 +0300

    17-01-2018

--- PersonalMirror/Scripts/movesquare.js
+++ PersonalMirror/Scripts/movesquare.js
@@ -2,6 +2,17 @@
     //var articleDiv = document.querySelector("div.article");
     //document.getElementById("square-empty").id = "square";
     //event.target.id = "square-empty";
-    event.target.id = "square";
+    event.target.id = "square1";
 }
-document.getElementById("square-empty").addEventListener("click", move);
+//document.getElementById("square-empty").addEventListener("click", move);
+//var elements = document.querySelectorAll("#field.row div");
+//for (var el in elements) {
+//    el.id = "aa";
+//    console.log(el.innerText);
+//    //el.addEventListener("click", move);
+//}
+var elems = document.querySelectorAll("#field.row div");
+
+for (var i = 0; i < elems.length; i++) {
+    console.log("Текст селектора " + i + ": " + elems[i].innerText);
+}
