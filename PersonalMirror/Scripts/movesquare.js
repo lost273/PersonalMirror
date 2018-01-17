ab134190654c49e139e06dd268e42404af8896ab
lost273 <dnk273@gmail.com>
2018-01-17 09:57:42 +0300

    17-01-2018

--- PersonalMirror/Scripts/movesquare.js
+++ PersonalMirror/Scripts/movesquare.js
@@ -1,4 +1,5 @@
-﻿function move(obj) {
+﻿document.getElementById("square-empty").addEventListener("click", move(this));
+function move(obj) {
     //var articleDiv = document.querySelector("div.article");
     document.getElementById("square-empty").id = "square";
     obj.id = "square-empty";
