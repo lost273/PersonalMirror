80db1ebb89125ffde174049048fe54b63c5be81e
lost273 <dnk273@gmail.com>
2018-01-21 19:33:34 +0300

    21-01

--- PersonalMirror/Scripts/myscript.js
+++ PersonalMirror/Scripts/myscript.js
@@ -5,6 +5,7 @@ var floatArray = [30.5, 10.02, 56.17, -1.01, 7.66, 5.17, 1000.33, -500.9];
 var sort = function (a) {
 
     for (var i = 0; i < a.length; i++) {
+        setTimeout(function () { document.getElementById(0).style.background = "#FF0000"; }, 5000);
         for (var j = a.length - 1; j > 0; j--) {
             if (a[j - 1] > a[j]) {
                 var temp;
