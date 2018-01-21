64c2f9280c44ab1ef96d86ec4cab93d152bba707
lost273 <dnk273@gmail.com>
2018-01-21 20:44:48 +0300

    21-01

--- PersonalMirror/Scripts/myscript.js
+++ PersonalMirror/Scripts/myscript.js
@@ -1,21 +1,7 @@
-﻿var intArray = [99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49];
+﻿var a = [99, -10, 10012, 18, -978, 5623, 463, -9, 287, 49];
 var charArray = ['b', 'a', 't', 'g', 'e', 'm', 'r', 'q', 'c'];
 var floatArray = [30.5, 10.02, 56.17, -1.01, 7.66, 5.17, 1000.33, -500.9];
 
-var sort = function (a) {
-
-    for (var i = 0; i < a.length; i++) {
-        for (var j = a.length - 1; j > 0; j--) {
-            setTimeout(display(a,i), 5000);
-            if (a[j - 1] > a[j]) {
-                var temp;
-                temp = a[j - 1];
-                a[j - 1] = a[j];
-                a[j] = temp;
-            }
-        }
-    }
-}
 
 var display = function (a, one) {
     document.getElementById(one).style.background = "#FF0000";
@@ -24,4 +10,13 @@ var display = function (a, one) {
     }
 }
 
-sort(intArray);
+for (var i = 0; i < a.length; i++) {
+    for (var j = a.length - 1; j > 0; j--) {
+        if (a[j - 1] > a[j]) {
+            var temp;
+            temp = a[j - 1];
+            a[j - 1] = a[j];
+            a[j] = temp;
+        }
+    }
+}
