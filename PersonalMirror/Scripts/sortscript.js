e1811a6ae8a942ba149f3cf0850eb379250efd25
lost273 <dnk273@gmail.com>
2018-01-21 20:58:05 +0300

    21-01

--- PersonalMirror/Scripts/sortscript.js
+++ PersonalMirror/Scripts/sortscript.js
@@ -2,13 +2,14 @@
 var charArray = ['b', 'a', 't', 'g', 'e', 'm', 'r', 'q', 'c'];
 var floatArray = [30.5, 10.02, 56.17, -1.01, 7.66, 5.17, 1000.33, -500.9];
 
-
 var display = function (a, one) {
     document.getElementById(one).style.background = "#FF0000";
     for (var k = 0; k < a.length; k++) {
         document.getElementById(k).innerHTML = a[k];
     }
 }
+
+display(a,1);
 
 for (var i = 0; i < a.length; i++) {
     for (var j = a.length - 1; j > 0; j--) {
