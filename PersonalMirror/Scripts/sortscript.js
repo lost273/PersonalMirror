d1b96c4d0441b0b8eed9283a1b860af97c7c1f81
lost273 <dnk273@gmail.com>
2018-01-22 20:23:52 +0300

    22-01

--- PersonalMirror/Scripts/sortscript.js
+++ PersonalMirror/Scripts/sortscript.js
@@ -5,16 +5,18 @@ var floatArray = [30.5, 10.02, 56.17, -1.01, 7.66, 5.17, 1000.33, -500.9];
 function display() {
     var i = +document.getElementById("i").innerHTML;
     var j = +document.getElementById("j").innerHTML;
-    var attribute = document.getElementById("attribute").innerHTML;
+ 
 
     //begin of the outer iteration
     if (j === -1) {
         j = a.length - 1;
+        i++;
     }
     if (i >= a.length) return;
     
     document.getElementById(i).style.background = "#FF0000";
     document.getElementById(j).style.background = "#32CD32";
+
     if (a[j - 1] > a[j]) {
         var temp;
         temp = a[j - 1];
@@ -24,11 +26,10 @@ function display() {
     for (var k = 0; k < a.length; k++) {
         document.getElementById(k).innerHTML = a[k];
     }
-    if (attribute === "true") { j++; }
-    else { j = 0; }
+    j--;
     document.getElementById("i").innerHTML = i;
     document.getElementById("j").innerHTML = j;
-    document.getElementById("attribute").innerHTML = attribute;
+    
 }
 
 document.getElementById("nextiteration").addEventListener("click", display);
