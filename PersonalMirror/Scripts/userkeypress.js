48cc956fac32c2f18ed2847034e21794b760af57
lost273 <dnk273@gmail.com>
2018-02-03 20:35:19 +0300

    03-02

--- PersonalMirror/Scripts/userkeypress.js
+++ PersonalMirror/Scripts/userkeypress.js
@@ -1,19 +1,19 @@
 ﻿document.addEventListener("keypress", getChar, false);
 function getChar(event) {
     if (event.which == null) { // IE
-        if (event.keyCode < 32) return null; // special chars
         if (event.keyCode === 13) { //enter
             userSay();
             return null;
         }
+        if (event.keyCode < 32) return null; // special chars
         document.getElementById("chat").innerHTML += String.fromCharCode(event.keyCode);
     }
     if (event.which != 0 && event.charCode != 0) { // all without IE
-        if (event.which < 32) return null; // special char
         if (event.keyCode === 13) { //enter
             userSay();
             return null;
         }
+        if (event.which < 32) return null; // special char
         document.getElementById("chat").innerHTML += String.fromCharCode(event.which); // other
     }
     return null; // special char
@@ -21,15 +21,14 @@ function getChar(event) {
 function userSay() {
     var xhr = new XMLHttpRequest();
 
-    var body = 'name=' + encodeURIComponent(name) +
-        '&surname=' + encodeURIComponent(surname);
+    var body = document.getElementById("chat").innerHTML;
 
-    xhr.open("POST", 'Index', true);
+    xhr.open("POST", '', true);
     xhr.setRequestHeader('Content-Type', 'text-plain');
 
     xhr.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            document.getElementById("chat").innerHTML += xmlhttp.responseText;
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            document.getElementById("chat").innerHTML += xhr.responseText;
         }
     };
 
