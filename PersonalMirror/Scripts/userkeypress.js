23a3d585f5f156e72b75394f9a37c538f4319d7a
lost273 <dnk273@gmail.com>
2018-02-03 18:59:44 +0300

    03-02

--- PersonalMirror/Scripts/userkeypress.js
+++ PersonalMirror/Scripts/userkeypress.js
@@ -2,10 +2,12 @@
 function getChar(event) {
     if (event.which == null) { // IE
         if (event.keyCode < 32) return null; // special chars
+        //enter
         document.getElementById("chat").innerHTML += String.fromCharCode(event.keyCode);
     }
     if (event.which != 0 && event.charCode != 0) { // all without IE
         if (event.which < 32) return null; // special char
+        //enter
         document.getElementById("chat").innerHTML += String.fromCharCode(event.which); // other
     }
     return null; // special char
