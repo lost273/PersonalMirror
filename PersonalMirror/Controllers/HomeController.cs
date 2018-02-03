7f12e07143115e7c478f8e17f1c602dd0de55cca
lost273 <dnk273@gmail.com>
2018-02-03 20:02:21 +0300

    03-02

--- PersonalMirror/Controllers/HomeController.cs
+++ PersonalMirror/Controllers/HomeController.cs
@@ -8,35 +8,41 @@ using System.Web.Mvc;
 namespace PersonalMirror.Controllers {
     public class HomeController : Controller {
         static ChatModel chatModel;
-        public ActionResult Index(string chatMessage) {
-            try {
-                if (chatModel == null) chatModel = new ChatModel();
+        [HttpGet]
+        public ActionResult Index() {
+            //try {
+            //    if (chatModel == null) chatModel = new ChatModel();
 
-                //leave only 10 messages
-                if (chatModel.Messages.Count > 100)
-                    chatModel.Messages.RemoveRange(0, 90);
+            //    //leave only 10 messages
+            //    if (chatModel.Messages.Count > 100)
+            //        chatModel.Messages.RemoveRange(0, 90);
 
-                // if simple request
-                if (!Request.IsAjaxRequest()) {
-                    return View(chatModel);
-                }
-                else {
-                    
-                    // add new message
-                    if (!string.IsNullOrEmpty(chatMessage)) {
-                        chatModel.Messages.Add(new ChatMessage() {
-                            Text = chatMessage,
-                            Date = DateTime.Now
-                        });
-                    }
+            //    // if simple request
+            //    if (!Request.IsAjaxRequest()) {
+            //        return View(chatModel);
+            //    }
+            //    else {
 
-                    return PartialView("History", chatModel);
-                }
-            }
-            catch (Exception ex) {
-                Response.StatusCode = 500;
-                return Content(ex.Message);
-            }
+            //        // add new message
+            //        if (!string.IsNullOrEmpty(chatMessage)) {
+            //            chatModel.Messages.Add(new ChatMessage() {
+            //                Text = chatMessage,
+            //                Date = DateTime.Now
+            //            });
+            //        }
+
+            //        return PartialView("History", chatModel);
+            //    }
+            //}
+            //catch (Exception ex) {
+            //    Response.StatusCode = 500;
+            //    return Content(ex.Message);
+            //}
+            return View(chatModel);
+        }
+        [HttpPost]
+        public string Index(string chatMessage) {
+            return "Hello!";
         }
     }
 }
--- PersonalMirror/Scripts/userkeypress.js
+++ PersonalMirror/Scripts/userkeypress.js
@@ -2,13 +2,36 @@
 function getChar(event) {
     if (event.which == null) { // IE
         if (event.keyCode < 32) return null; // special chars
-        //enter
+        if (event.keyCode === 13) { //enter
+            userSay();
+            return null;
+        }
         document.getElementById("chat").innerHTML += String.fromCharCode(event.keyCode);
     }
     if (event.which != 0 && event.charCode != 0) { // all without IE
         if (event.which < 32) return null; // special char
-        //enter
+        if (event.keyCode === 13) { //enter
+            userSay();
+            return null;
+        }
         document.getElementById("chat").innerHTML += String.fromCharCode(event.which); // other
     }
     return null; // special char
+}
+function userSay() {
+    var xhr = new XMLHttpRequest();
+
+    var body = 'name=' + encodeURIComponent(name) +
+        '&surname=' + encodeURIComponent(surname);
+
+    xhr.open("POST", 'Index', true);
+    xhr.setRequestHeader('Content-Type', 'text-plain');
+
+    xhr.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            document.getElementById("chat").innerHTML += xmlhttp.responseText;
+        }
+    };
+
+    xhr.send(body);
 }
