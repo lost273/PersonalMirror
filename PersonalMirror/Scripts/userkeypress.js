3a8ef9555527c1c8a8f5b71b0d63832a3dd600ac
lost273 <dnk273@gmail.com>
2018-02-04 13:35:43 +0300

    04-02

--- PersonalMirror/Scripts/userkeypress.js
+++ PersonalMirror/Scripts/userkeypress.js
@@ -6,7 +6,7 @@ function getChar(event) {
             return null;
         }
         if (event.keyCode < 32) return null; // special chars
-        document.getElementById("chat").innerHTML += String.fromCharCode(event.keyCode);
+        document.getElementById("usermessage").innerHTML += String.fromCharCode(event.keyCode);
     }
     if (event.which != 0 && event.charCode != 0) { // all without IE
         if (event.keyCode === 13) { //enter
@@ -14,21 +14,21 @@ function getChar(event) {
             return null;
         }
         if (event.which < 32) return null; // special char
-        document.getElementById("chat").innerHTML += String.fromCharCode(event.which); // other
+        document.getElementById("usermessage").innerHTML += String.fromCharCode(event.which); // other
     }
     return null; // special char
 }
 function userSay() {
     var xhr = new XMLHttpRequest();
 
-    var body = { 'message': document.getElementById("chat").innerHTML };
+    var body = { 'message': document.getElementById("usermessage").innerHTML };
 
     xhr.open("POST", '/Home/Index', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
 
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            document.getElementById("chat").innerHTML += xhr.responseText;
+            document.getElementById("usermessage").innerHTML += xhr.responseText;
         }
     };
 
