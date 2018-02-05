c23872ecb5108a40f7db33db23407aadefc3d6a2
lost273 <dnk273@gmail.com>
2018-02-05 21:06:09 +0300

    05-02

--- PersonalMirror/Controllers/HomeController.cs
+++ PersonalMirror/Controllers/HomeController.cs
@@ -7,38 +7,9 @@ using System.Web.Mvc;
 
 namespace PersonalMirror.Controllers {
     public class HomeController : Controller {
-        static ChatModel chatModel;
         [HttpGet]
         public ActionResult Index() {
-            //try {
-            //    if (chatModel == null) chatModel = new ChatModel();
-
-            //    //leave only 10 messages
-            //    if (chatModel.Messages.Count > 100)
-            //        chatModel.Messages.RemoveRange(0, 90);
-
-            //    // if simple request
-            //    if (!Request.IsAjaxRequest()) {
-            //        return View(chatModel);
-            //    }
-            //    else {
-
-            //        // add new message
-            //        if (!string.IsNullOrEmpty(chatMessage)) {
-            //            chatModel.Messages.Add(new ChatMessage() {
-            //                Text = chatMessage,
-            //                Date = DateTime.Now
-            //            });
-            //        }
-
-            //        return PartialView("History", chatModel);
-            //    }
-            //}
-            //catch (Exception ex) {
-            //    Response.StatusCode = 500;
-            //    return Content(ex.Message);
-            //}
-            return View(chatModel);
+            return View();
         }
         [HttpPost]
         public string Index(string message) {
