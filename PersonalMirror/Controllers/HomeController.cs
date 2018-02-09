35d035aae8813fbaf439e13f51db48dec6aade3f
lost273 <dnk273@gmail.com>
2018-02-09 20:10:28 +0300

    09-02

--- PersonalMirror/Controllers/HomeController.cs
+++ PersonalMirror/Controllers/HomeController.cs
@@ -17,26 +17,31 @@ using PersonalMirror.Models;
 namespace PersonalMirror.Controllers {
     public class HomeController : Controller {
         VocabularyContext db = new VocabularyContext();
+        Dictionary<string, string> userSentence = new Dictionary<string, string>();
         [HttpGet]
         public ActionResult Index() {
             return View();
         }
         [HttpPost]
         public string Index(string message) {
-            string[] UserWords;
-            UserWords = message.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            IdentifyNewWords(UserWords);
+            string[] userWords;
+            userWords = message.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            IdentifyNewWords(userWords);
             IsCommand(UserWords);
             return "servermessage";
         }
         //is this message contains the new words
         public string[] IdentifyNewWords(string[] text) {
+            bool isImperative = true;
             foreach (string s in text) {
                 if (db.Nouns.FirstOrDefault(word => word.Name == s) != null) {
                     Behavior(s, "noun");
                 }
                 if(db.Verbs.FirstOrDefault(word => word.Name == s) != null) {
-                    Behavior(s, "verb");
+                    if (isImperative) {
+                        Behavior(s, "imperative");
+                    } else
+                        Behavior(s, "verb");
                 }
                 if (db.Pronouns.FirstOrDefault(word => word.Name == s) != null) {
                     Behavior(s, "pronoun");
@@ -44,6 +49,7 @@ namespace PersonalMirror.Controllers {
                 if (db.Adjectives.FirstOrDefault(word => word.Name == s) != null) {
                     Behavior(s, "adjective");
                 }
+                isImperative = false;
             }
             //MakeQueryForUser for add new words in the database
             return new string[5];
@@ -54,7 +60,7 @@ namespace PersonalMirror.Controllers {
         }
         //AI behavior
         public void Behavior(string userWord, string particle) {
-
+            userSentence.Add(particle, userWord);
         }
     }
 }
