3dd89e53c2ef45a88b3a2a13ed734fcae1ef2f68
lost273 <dnk273@gmail.com>
2018-02-14 20:00:05 +0300

    14-02

---
+++ PersonalMirror/App_Start/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin;
+using Owin;
+using PersonalMirror.Models;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity;
+
+[assembly: OwinStartup(typeof(PersonalMirror.App_Start.Startup))]
+
+namespace PersonalMirror.App_Start {
+    public class Startup {
+        public void Configuration(IAppBuilder app) {
+            
+            app.CreatePerOwinContext<ApplicationContext>(ApplicationContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+        }
+    }
+}
