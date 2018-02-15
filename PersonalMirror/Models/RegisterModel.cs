398d848bc6a3cb0992a6c30c2898869120f93129
lost273 <dnk273@gmail.com>
2018-02-15 15:13:19 +0300

    15-02

--- PersonalMirror/Models/RegisterModel.cs
+++ PersonalMirror/Models/RegisterModel.cs
@@ -17,7 +17,7 @@ namespace PersonalMirror.Models {
         public string Password { get; set; }
 
         [Required]
-        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
         [DataType(DataType.Password)]
         public string PasswordConfirm { get; set; }
     }
